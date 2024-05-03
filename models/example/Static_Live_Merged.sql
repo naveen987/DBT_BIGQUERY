82a53c4a8579fcb3d5f86eb8640bddffb30271e0
Tumakuru shreeshylesha Nataraju <natarajuts43@gmail.com>
2024-05-03 09:01:48 +0000

    merged query updated with missing columns

--- models/example/Static_Live_Merged.sql
+++ models/example/Static_Live_Merged.sql
@@ -11,7 +11,9 @@ SELECT
   a.Low AS Low_Static, 
   a.`Adj-close`,
   CAST(a.Date AS DATE) AS Date,  -- Ensure Date is of type DATE
-  a.Volume AS Volume_Static
+  a.Volume AS Volume_Static,
+  NULL AS Number_of_Transactions,  -- Placeholder for Number_of_Transactions
+  NULL AS Volume_Weighted_Average_Price  -- Placeholder for Volume_Weighted_Average_Price
 FROM `dbt_tnataraju.Combined_static_data` a
 
 UNION ALL
@@ -19,11 +21,13 @@ UNION ALL
 SELECT 
   'Transformed' AS source,
   b.Ticker,
-  NULL AS Open, 
+  NULL AS Open,  -- No Open data in Transformed
   b.Close AS Close_Transformed,
   b.High AS High_Transformed,
   b.Low AS Low_Transformed,
-  NULL AS `Adj-close`, 
+  NULL AS `Adj-close`,  -- No Adj-close data in Transformed
   CAST(b.Date AS DATE) AS Date,  -- Ensure Date is of type DATE
-  b.Volume AS Volume_Transformed
+  b.Volume AS Volume_Transformed,
+  b.Number_of_Transactions,
+  b.Volume_Weighted_Average_Price
 FROM `dbt_tnataraju.Stock_data_transformation` b
