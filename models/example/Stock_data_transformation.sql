a846f4e61109a274fefe4367d1b4962970ccdd3a
Tumakuru shreeshylesha Nataraju <natarajuts43@gmail.com>
2024-05-03 08:40:43 +0000

    Live date pulled frm the api and transformed

--- models/example/Stock_data_transformation.sql
+++ models/example/Stock_data_transformation.sql
@@ -1,20 +1,18 @@
--- models/transformed_stock_data.sql
+{{ config(materialized='table', table_name='Live_data_Transformed') }}
 
-{{ config(materialized='table') }}
-
-WITH renamed_columns AS (
+WITH formatted_columns AS (
   SELECT 
-    CAST(CASE WHEN ABS(Open_Price * 100 - ROUND(Open_Price * 100)) < 0.01 THEN Open_Price ELSE ROUND(Open_Price, 2) END AS FLOAT64) AS Price,
-    CAST(CASE WHEN ABS(Close_Price * 100 - ROUND(Close_Price * 100)) < 0.01 THEN Close_Price ELSE ROUND(Close_Price, 2) END AS FLOAT64) AS Close,
-    CAST(CASE WHEN ABS(High_Price * 100 - ROUND(High_Price * 100)) < 0.01 THEN High_Price ELSE ROUND(High_Price, 2) END AS FLOAT64) AS High,
-    CAST(CASE WHEN ABS(Low_Price * 100 - ROUND(Low_Price * 100)) < 0.01 THEN Low_Price ELSE ROUND(Low_Price, 2) END AS FLOAT64) AS Low,
+    CAST(CASE WHEN ABS(Open * 100 - ROUND(Open * 100)) < 0.01 THEN Open ELSE ROUND(Open, 2) END AS FLOAT64) AS Open,
+    CAST(CASE WHEN ABS(Close * 100 - ROUND(Close * 100)) < 0.01 THEN Close ELSE ROUND(Close, 2) END AS FLOAT64) AS Close,
+    CAST(CASE WHEN ABS(High * 100 - ROUND(High * 100)) < 0.01 THEN High ELSE ROUND(High, 2) END AS FLOAT64) AS High,
+    CAST(CASE WHEN ABS(Low * 100 - ROUND(Low * 100)) < 0.01 THEN Low ELSE ROUND(Low, 2) END AS FLOAT64) AS Low,
     CAST(ROUND(Volume_Weighted_Average_Price, 2) AS FLOAT64) AS Volume_Weighted_Average_Price,
-    Volume,  -- Formats Volume with commas as thousand separators
+    Volume,  -- Keeping Volume as it is
     CAST(Number_of_Transactions AS INT64) AS Number_of_Transactions,
     CAST(Ticker AS STRING) AS Ticker,
     Date
   FROM 
-    dbt_tnataraju.stock_data
+    dbt_tnataraju.from_gcs
 )
 
-SELECT * FROM renamed_columns
+SELECT * FROM formatted_columns
